58c9508b7d22ab6b0ee31eee22ad45af7916847b
Yusen Wang <117229722+tonywang0418@users.noreply.github.com>
2024-01-05 21:47:00 -0800

    Add files via upload

---
+++ MyTemplate (1).py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# <DESCRIPTION OF SCRIPT>
+# Ski-20130930: Initial version
+
+# Set up initial variables and imports
+
+
+# Main routine that is called when script is run
+def main():
+   """WHAT DOES THIS FUNCTION DO"""
+
+
+def function1(message):
+   """WHAT DOES THIS FUNCTION DO"""
+
+
+# Run main() if script called directly, else use as a library to be imported
+if __name__ == "__main__":
+   main()
