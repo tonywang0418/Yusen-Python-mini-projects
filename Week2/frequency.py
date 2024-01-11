2d0366982c33826a553dd9b9990482091e7ebc12
YusenWang <wangyusen2012@gmail.com>
2024-01-11 15:45:27 -0800

    Add more text and changed the way of count by using empty dictionary

--- Week2/frequency.py
+++ Week2/frequency.py
@@ -2,48 +2,30 @@
 # This script will count the number of vowels and consonants in a text.
 # YW-20240109
 
-TEXT = "Yes and No are not the only answers. In the intricate and sometimes perplexing realm of security, the straightforward ‘yes’ and ‘no’ answers we’re accustomed to may suddenly seem elusive. It’s a humorous take on the intricacies of security, where the right answer might be 'Well, it depends,' and navigating the security landscape feels a bit like solving a riddle. Welcome to the world where black and white answers are a rare find and shades of gray are supreme. The majority of security breaches are caused by human error. Even though you send out reminder emails and try to teach everyone the cybersecurity safety practices, because humans are evolved to trust people, someone will always trust the wrong person causing a breach. It’s like trying to teach a goldfish to play fetch. So, while you keep fighting the good fight against digital chaos, you secretly wish that human error would take a vacation."  
+TEXT = "Yes and No are not the only answers. In the intricate and sometimes perplexing realm of security, the straightforward ‘yes’ and ‘no’ answers we’re accustomed to may suddenly seem elusive. It’s a humorous take on the intricacies of security, where the right answer might be 'Well, it depends,' and navigating the security landscape feels a bit like solving a riddle. Welcome to the world where black and white answers are a rare find and shades of gray are supreme. The majority of security breaches are caused by human error. Even though you send out reminder emails and try to teach everyone the cybersecurity safety practices, because humans are evolved to trust people, someone will always trust the wrong person causing a breach. It’s like trying to teach a goldfish to play fetch. So, while you keep fighting the good fight against digital chaos, you secretly wish that human error would take a vacation. Todays tip: MFA enabled? Make sure you have it enabled and not someone else!! In a world where cyberattacks are the uninvited party crashers, MFA is the door policy we can all get behind even if it does involve a lot of training and begging to get people to use it. Clicking on a phishing link is like taking a stroll through a cyberjungle and stumbling upon a sign that says, “Free treasure ahead!” It’s as if your computer suddenly transformed into an adventurous pirate ship, setting sail on the high seas of the internet. But here’s the catch: instead of buried treasure, you find yourself face-to-face with malware that’s more mischievous than a band of pirate monkeys raiding your digital coconuts."  
+text_low = TEXT.lower()
+vowels = "aeiou" 
+vowels_count = {}
+consonants_count = {}
 
-def count_vowels(TEXT):
-    """The following function is use to count vowels, whenever the script finds one it will +1 """
-    TEXT_lower = TEXT.lower() #This is a way to treat captial letter as lowercase letter
-    vowels = "aeiou"
+for char in text_low: #use for loop to count each letter and save it to the dictionary" 
+    if char.isalpha():
+        if char in vowels:
+            vowels_count[char] = vowels_count.get(char, 0) + 1 #assign value to the key. if there is value then +1, if no value yet, it starts from 0 and +1
+        else:
+            consonants_count[char] = consonants_count.get(char, 0) + 1
     
-    vowel_count = 0 
 
-    for letter in TEXT_lower:
-        if letter in vowels:
-            vowel_count += 1
-    
-    return vowel_count
-def count_consonants(text):
-    """The following function is use to count consonants, whenever the script finds one it will +1 """
-    TEXT_lower = TEXT.lower()  #This is a way to treat captial letter as lowercase letter
-    consonants = "bcdfghjklmnpqrstvwxyz"
-    
-    consonants_count = 0
+def sum_calculate(dictionary):
+    """sum calculation when I put dictionary in ()"""
+    return sum(dictionary.values())
 
-    for letter in TEXT_lower:
-        if letter in consonants:
-            consonants_count += 1
-
-    return consonants_count
 def main():
-    """I created empty dictionaries for vowels and consonants as required"""
-#This is just my orignal script but since the requirement is to have two empty dictionaries so I have to write a new one.
-    #vowels = {"letter": ["a", "e", "i", "o", "u"] }
-    #consonants = {"letter": ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y"]}
-    #count_v = sum(TEXT_lower.count(letter) for letter in vowels["letter"])
-    #count_c = sum(TEXT_lower.count(letter) for letter in consonants["letter"]) 
-
-
-    vowels = {}
-    consonants = {}
-    vowels["count"] = count_vowels(TEXT) # Adding key"count" and value "count_vowels(TEXT)" to the dictionary"
-    consonants["count"] = count_consonants(TEXT)
-    print(f'vowels= {vowels["count"]}')
-    print(f'consonants= {consonants["count"]}')
-
+    """print sum and dictionaries out"""
+    print(f'vowels= {sum_calculate(vowels_count)}')
+    print(f'consonants= {sum_calculate(consonants_count)}')
+    print(f'vowels= {vowels_count}')
+    print(f'consonants= {consonants_count}')
 if __name__ == "__main__":
     main()
 
