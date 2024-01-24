1e14bb2e48f02be864defd9ddfb1e6937e99a380
YusenWang <wangyusen2012@gmail.com>
2024-01-24 15:37:39 -0800

    Script takes one argument on the command line like so: ping2.py <filename | IP | Domainname>

---
+++ Week4/ping2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# Script takes one arguments on the command line like so: ping1.py <filename>
+# YW-20240124
+from pinglib import pingthis
+import sys
+
+def main():
+    """ open the file in read mode and .read and return as a list. Finally print output """
+    if len(sys.argv) != 2:
+        print("Usage: ping1.py <filename | IP | Domainname>")
+        sys.exit()
+
+    filename = sys.argv[1]
+    try:
+        with open(filename, 'r') as file:
+            addresses = file.read().splitlines() # .read the file as a string and use splitlines to split the string at '\n' line break and made a list [].
+
+            print("IP, TimeToPing (ms)")
+            for line in addresses:
+                result = pingthis(line)     #apply ip address to my pinglib
+                print(f'{result[0]}, {result[1]}') #display in this format
+    except FileNotFoundError:
+        result = pingthis(filename)
+        print(f'{result[0]}, {result[1]}') # if file not found, treat it as IP or domain name.
+
+if __name__ == "__main__":
+    main()
